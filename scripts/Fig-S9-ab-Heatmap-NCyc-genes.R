6675233afda4118c222dcc6c3d2ec99e41273c27
MaryoHg <hg.maryo@gmail.com>
2022-08-07 17:15:05 -0500

    Update script
    update script

--- scripts/Fig-S9-ab-Heatmap-NCyc-genes.R
+++ scripts/Fig-S9-ab-Heatmap-NCyc-genes.R
@@ -1,42 +1,51 @@
-### Heatmap of the Nitrogen Cycle related genes
-###############################################
-rm (list = ls())
+### Figure S9-A,B: Heatmap of the Nitrogen Cycle related genes
+##############################################################
+options(scipen = 100000, digits = 3); rm (list = ls())
 set.seed(999)
-cal_z_score <- function(x){
-        (x - mean (x)) / sd (x)
-        }
+	zscorefunct <- function(x){
+	        (x - mean (x)) / sd (x)
+	        }
+	
+	relabpercfunt <- function (x) {
+	        (x/sum(x)) * 100}
 
-cal_percent <- function (x) {
-        (x/sum(x)) * 100
-        }
+metadata <- read.table('../data/metadata.txt',  header = T, stringsAsFactors = T, check.names = F)
 
-# 1. Loadint the dataframe: counts per each paired end metagenome (n = 8)
-        # Loading the raw data counts:
-        data <- read.delim('/Users/mariohg/Dropbox/Norma/data/NCyc-genes/nitro_genes.tsv', comment.char = "#", row.names = 1)
+# 1. Loading the data-frame: counts per each paired end metagenome (n = 8)
+        data <- read.delim('../data/functionality/nitro_genes.tsv', 
+        		   comment.char = "#", row.names = 1)
+        colnames(data) <- as.character(gsub(pattern = "X", replacement = "", colnames(data)))
+         
+        # summarizing data by replicate and treatment
         data_t <- data.frame(t(data))
-        
         meta_table <- data.frame(row.names=rownames(data_t), 
                                  t(as.data.frame(strsplit(rownames(data_t),"_"))))
+        data_N <- stats::aggregate(data_t, by = list(meta_table$X2, meta_table$X1), FUN = mean)
+        rownames(data_N) <- paste(data_N$Group.1, data_N$Group.2, sep = "_")
+        #data_N1 <- aggregate(data_t, by = list(meta_table$X2, meta_table$X3), mean)
         
-        data_N <- aggregate(data_t, by = list(meta_table$X2, meta_table$X3), mean)
+## 2. Nomamalizing the data according to: 
+        ## z-score (by gene)
+        data_z <- data.frame(t(apply(X = data_N[,-1:-2], MARGIN = 2, FUN = zscorefunct)))
+        data_z <- data_z %>%
+        	dplyr::filter(!is.na(Control_15))
         
-        
-        # Nomrmalizing the data according to:
-        # Determining the z-score centered dataframe by gene (colum):
-        data_z <- data.frame(t(apply(X = data_N[,-1:-2], MARGIN = 2, FUN = cal_z_score)))
-        
-        # Total percent by row (SampleID)
-        data_perc <- data.frame(apply(X = data_N[,-1:-2], MARGIN = 1, FUN = cal_percent))
+        ## relative abundance per sample (SampleID)
+        data_perc <- data.frame(apply(X = data_N[,-1:-2], MARGIN = 1, FUN = relabpercfunt))
         colSums(data_perc)
         
         
-# 2. Annotation metadata for heatmap
+## 3. Annotation metadata for heatmap
         
-        # Colums (samples) data
+        ## metadata for samples and their respective treatment
         col_anot <- data.frame ("Treatment" = colnames(data_z),
-                                "Metagenome" = rep (c("UNS", "MAS"), times =2), row.names = 1); col_anot
-        # Row annotation data
-        annotation <- read.delim('/Users/mariohg/Dropbox/Norma/data/NCyc-genes/annot_genes.tsv', row.names = 2)
+                                "Metagenome" = rep (c("UNS", "MAS"), 
+                                		    times =2), row.names = 1); col_anot
+        
+        # Row annotation data: 
+        ## load annotation for N pathways classification of genes
+        ## set colors for each pathway
+        annotation <- read.delim('../data/functionality/annot_genes.tsv', row.names = 2)
         annotation$Annotation <- NULL
 
         pathways <- RColorBrewer::brewer.pal(n = 9, name = "Set3"); pathways
@@ -46,20 +55,23 @@ cal_percent <- function (x) {
         heatmap_colors <- list (Metagenome = c("UNS" = "blue", "MAS" = "red"),
                                 Pathways = pathways); heatmap_colors
         
-# 3. Drawing the heatmap: z-score heatmap
+## 4. Drawing the heatmap: z-score heatmap
         # italic gene names:
         genes <- lapply(rownames(data_z), function (x) bquote(italic(.(x))))
         
-        # breaks of heatmap legend
+        # breaks fpr heatmap legend
         breaks = base::seq(from = -2, to = 2, by = 0.01)
         
-        pheatmap::pheatmap(mat = data_z, labels_row = as.expression(genes),
+        pheatmap::pheatmap(mat = data_z, 
+        		   labels_row = as.expression(genes),
                            cellwidth = 30, cellheight = 9,
                            annotation_col = col_anot,
                            annotation_row = annotation,
                            annotation_colors = heatmap_colors,
-                           show_colnames = F, treeheight_col = 25, treeheight_row = 25,
-                           cluster_rows = T, cluster_cols = T, cutree_cols = 2,
+                           show_colnames = F, 
+        		   treeheight_col = 25, treeheight_row = 25,
+                           cluster_rows = T, cluster_cols = T, 
+        		   cutree_cols = 2,
                            color = colorRampPalette(c("white", "yellow", "red"))(length(breaks)),
                            breaks = breaks, clustering_method = "average")
         
@@ -68,68 +80,68 @@ cal_percent <- function (x) {
         colnames(data_perc)
         data_perc <- within(data_perc, {
                 UNS1 <- 0
-                UNS1 [Control1 <=0.01]<- 0
-                UNS1 [Control1 > 0.01 & Control1 <= 0.05]<- 1
-                UNS1 [Control1 > 0.05 & Control1 <= 0.10]<- 2
-                UNS1 [Control1 > 0.10 & Control1 <= 0.20]<- 3
-                UNS1 [Control1 > 0.20 & Control1  <= 0.50]<- 4
-                UNS1 [Control1 > 0.50 & Control1 <= 1.00]<- 5
-                UNS1 [Control1 > 1.00 & Control1 <= 2.00]<- 6
-                UNS1 [Control1 > 2.00 & Control1 <= 4.00]<- 7
-                UNS1 [Control1 > 4.00 & Control1 <= 8.00]<- 8
-                UNS1 [Control1 > 8.00 & Control1 <= 12.00]<- 9
-                UNS1 [Control1 > 12.00 & Control1 <= 16.00]<- 10
-                UNS1 [Control1 > 16.00 & Control1 <= 20.00]<- 11
-                UNS1 [Control1 > 20.00]<- 12})
+                UNS1 [Control_15 <=0.01]<- 0
+                UNS1 [Control_15 > 0.01 & Control_15 <= 0.05]<- 1
+                UNS1 [Control_15 > 0.05 & Control_15 <= 0.10]<- 2
+                UNS1 [Control_15 > 0.10 & Control_15 <= 0.20]<- 3
+                UNS1 [Control_15 > 0.20 & Control_15  <= 0.50]<- 4
+                UNS1 [Control_15 > 0.50 & Control_15 <= 1.00]<- 5
+                UNS1 [Control_15 > 1.00 & Control_15 <= 2.00]<- 6
+                UNS1 [Control_15 > 2.00 & Control_15 <= 4.00]<- 7
+                UNS1 [Control_15 > 4.00 & Control_15 <= 8.00]<- 8
+                UNS1 [Control_15 > 8.00 & Control_15 <= 12.00]<- 9
+                UNS1 [Control_15 > 12.00 & Control_15 <= 16.00]<- 10
+                UNS1 [Control_15 > 16.00 & Control_15 <= 20.00]<- 11
+                UNS1 [Control_15 > 20.00]<- 12})
         
         data_perc <- within(data_perc, {
                 UNS2 <- 0
-                UNS2 [Control2 <=0.01]<- 0
-                UNS2 [Control2 > 0.01 & Control2 <= 0.05]<- 1
-                UNS2 [Control2 > 0.05 & Control2 <= 0.10]<- 2
-                UNS2 [Control2 > 0.10 & Control2 <= 0.20]<- 3
-                UNS2 [Control2 > 0.20 & Control2  <= 0.50]<- 4
-                UNS2 [Control2 > 0.50 & Control2 <= 1.00]<- 5
-                UNS2 [Control2 > 1.00 & Control2 <= 2.00]<- 6
-                UNS2 [Control2 > 2.00 & Control2 <= 4.00]<- 7
-                UNS2 [Control2 > 4.00 & Control2 <= 8.00]<- 8
-                UNS2 [Control2 > 8.00 & Control2 <= 12.00]<- 9
-                UNS2 [Control2 > 12.00 & Control2 <= 16.00]<- 10
-                UNS2 [Control2 > 16.00 & Control2 <= 20.00]<- 11
-                UNS2 [Control2 > 20.00]<- 12})
+                UNS2 [Control_25 <=0.01]<- 0
+                UNS2 [Control_25 > 0.01 & Control_25 <= 0.05]<- 1
+                UNS2 [Control_25 > 0.05 & Control_25 <= 0.10]<- 2
+                UNS2 [Control_25 > 0.10 & Control_25 <= 0.20]<- 3
+                UNS2 [Control_25 > 0.20 & Control_25  <= 0.50]<- 4
+                UNS2 [Control_25 > 0.50 & Control_25 <= 1.00]<- 5
+                UNS2 [Control_25 > 1.00 & Control_25 <= 2.00]<- 6
+                UNS2 [Control_25 > 2.00 & Control_25 <= 4.00]<- 7
+                UNS2 [Control_25 > 4.00 & Control_25 <= 8.00]<- 8
+                UNS2 [Control_25 > 8.00 & Control_25 <= 12.00]<- 9
+                UNS2 [Control_25 > 12.00 & Control_25 <= 16.00]<- 10
+                UNS2 [Control_25 > 16.00 & Control_25 <= 20.00]<- 11
+                UNS2 [Control_25 > 20.00]<- 12})
         
         
         data_perc <- within(data_perc, {
                 MAS1 <- 0
-                MAS1 [Maiz1 <=0.01]<- 0
-                MAS1 [Maiz1 > 0.01 & Maiz1 <= 0.05]<- 1
-                MAS1 [Maiz1 > 0.05 & Maiz1 <= 0.10]<- 2
-                MAS1 [Maiz1 > 0.10 & Maiz1 <= 0.20]<- 3
-                MAS1 [Maiz1 > 0.20 & Maiz1  <= 0.50]<- 4
-                MAS1 [Maiz1 > 0.50 & Maiz1 <= 1.00]<- 5
-                MAS1 [Maiz1 > 1.00 & Maiz1 <= 2.00]<- 6
-                MAS1 [Maiz1 > 2.00 & Maiz1 <= 4.00]<- 7
-                MAS1 [Maiz1 > 4.00 & Maiz1 <= 8.00]<- 8
-                MAS1 [Maiz1 > 8.00 & Maiz1 <= 12.00]<- 9
-                MAS1 [Maiz1 > 12.00 & Maiz1 <= 16.00]<- 10
-                MAS1 [Maiz1 > 16.00 & Maiz1 <= 20.00]<- 11
-                MAS1 [Maiz1 > 20.00]<- 12})
+                MAS1 [Maiz_25 <=0.01]<- 0
+                MAS1 [Maiz_25 > 0.01 & Maiz_25 <= 0.05]<- 1
+                MAS1 [Maiz_25 > 0.05 & Maiz_25 <= 0.10]<- 2
+                MAS1 [Maiz_25 > 0.10 & Maiz_25 <= 0.20]<- 3
+                MAS1 [Maiz_25 > 0.20 & Maiz_25  <= 0.50]<- 4
+                MAS1 [Maiz_25 > 0.50 & Maiz_25 <= 1.00]<- 5
+                MAS1 [Maiz_25 > 1.00 & Maiz_25 <= 2.00]<- 6
+                MAS1 [Maiz_25 > 2.00 & Maiz_25 <= 4.00]<- 7
+                MAS1 [Maiz_25 > 4.00 & Maiz_25 <= 8.00]<- 8
+                MAS1 [Maiz_25 > 8.00 & Maiz_25 <= 12.00]<- 9
+                MAS1 [Maiz_25 > 12.00 & Maiz_25 <= 16.00]<- 10
+                MAS1 [Maiz_25 > 16.00 & Maiz_25 <= 20.00]<- 11
+                MAS1 [Maiz_25 > 20.00]<- 12})
         
         data_perc <- within(data_perc, {
                 MAS2 <- 0
-                MAS2 [Maiz2 <=0.01]<- 0
-                MAS2 [Maiz2 > 0.01 & Maiz2 <= 0.05]<- 1
-                MAS2 [Maiz2 > 0.05 & Maiz2 <= 0.10]<- 2
-                MAS2 [Maiz2 > 0.10 & Maiz2 <= 0.20]<- 3
-                MAS2 [Maiz2 > 0.20 & Maiz2  <= 0.50]<- 4
-                MAS2 [Maiz2 > 0.50 & Maiz2 <= 1.00]<- 5
-                MAS2 [Maiz2 > 1.00 & Maiz2 <= 2.00]<- 6
-                MAS2 [Maiz2 > 2.00 & Maiz2 <= 4.00]<- 7
-                MAS2 [Maiz2 > 4.00 & Maiz2 <= 8.00]<- 8
-                MAS2 [Maiz2 > 8.00 & Maiz2 <= 12.00]<- 9
-                MAS2 [Maiz2 > 12.00 & Maiz2 <= 16.00]<- 10
-                MAS2 [Maiz2 > 16.00 & Maiz2 <= 20.00]<- 11
-                MAS2 [Maiz2 > 20.00]<- 12})
+                MAS2 [Maiz_35 <=0.01]<- 0
+                MAS2 [Maiz_35 > 0.01 & Maiz_35 <= 0.05]<- 1
+                MAS2 [Maiz_35 > 0.05 & Maiz_35 <= 0.10]<- 2
+                MAS2 [Maiz_35 > 0.10 & Maiz_35 <= 0.20]<- 3
+                MAS2 [Maiz_35 > 0.20 & Maiz_35  <= 0.50]<- 4
+                MAS2 [Maiz_35 > 0.50 & Maiz_35 <= 1.00]<- 5
+                MAS2 [Maiz_35 > 1.00 & Maiz_35 <= 2.00]<- 6
+                MAS2 [Maiz_35 > 2.00 & Maiz_35 <= 4.00]<- 7
+                MAS2 [Maiz_35 > 4.00 & Maiz_35 <= 8.00]<- 8
+                MAS2 [Maiz_35 > 8.00 & Maiz_35 <= 12.00]<- 9
+                MAS2 [Maiz_35 > 12.00 & Maiz_35 <= 16.00]<- 10
+                MAS2 [Maiz_35 > 16.00 & Maiz_35 <= 20.00]<- 11
+                MAS2 [Maiz_35 > 20.00]<- 12})
         
         # annotation:
         # Colums (samples) data percent
