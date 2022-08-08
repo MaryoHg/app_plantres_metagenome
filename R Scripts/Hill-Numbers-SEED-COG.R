9277cbf91003589200be1fd716fd1d9fdbb407b1
MaryoHg <hg.maryo@gmail.com>
2022-08-08 00:14:09 -0500

    update for no reason

--- R Scripts/Hill-Numbers-SEED-COG.R
+++ R Scripts/Hill-Numbers-SEED-COG.R
@@ -4,33 +4,20 @@ set.seed(999)
 library(ggplot2)
 library(hilldiv)
 
-
-## tutorial
-data(bat.diet.otutable); str (bat.diet.otutable)
-data(bat.diet.tree); str (bat.diet.tree)
-data(bat.diet.hierarchy); str (bat.diet.hierarchy)
-
-hilldiv::div_test(countable = bat.diet.otutable, qvalue = 0, hierarchy = bat.diet.hierarchy)
-
-hilldiv::div_test(countable = bat.diet.otutable,
-		  qvalue = 1,
-		  hierarchy = bat.diet.hierarchy,
-		  posthoc = TRUE)
-
 ## analyzing the data from the SEED functional data
 	## Reading the OTU table: rows are functions and columns are samples
 	## Uncomment the functional level you need to test:
 	#data <- read.delim('/Users/mariohg/Dropbox/Norma/data/Metagenome/SEED/SEED_L1_COUNT.txt')
-	data <- read.delim('/Users/mariohg/Dropbox/Norma/data/Metagenome/SEED/SEED_L2_COUNT.txt')
-	rownames(data) <- data$Function; data$Function<-NULL
-	
+	data <- read.delim('../data/functionality/SEED_L2_COUNT.txt', row.names = 1)[,1:4]; head(data)
+
 	## Hierarchy or mapping file
-	mapping <- read.delim('/Users/mariohg/Dropbox/Norma/data/mapping.txt')[,-2]; mapping
-	
+	metadata <- read.delim('../data/metadata.txt', check.names = F)[,c(5,3)]; head(metadata)
+
 	## Testing different q orders: uncomment the needed
-	hilldiv::div_test(countable = data, qvalue = 0, hierarchy = mapping)
-	hilldiv::div_test(countable = data, qvalue = 1, hierarchy = mapping)
-	hilldiv::div_test(countable = data, qvalue = 2, hierarchy = mapping)
+	## first column of metadata must be colnmaes of "data"
+	hilldiv::div_test(countable = data, qvalue = 0, hierarchy = metadata)
+	hilldiv::div_test(countable = data, qvalue = 1, hierarchy = metadata)
+	hilldiv::div_test(countable = data, qvalue = 2, hierarchy = metadata)
 	
 
 ## analyzing the data from the SEED functional data
@@ -41,12 +28,12 @@ hilldiv::div_test(countable = bat.diet.otutable,
 	rownames(data) <- data[,1]; data[,1]<-NULL
 	
 	## Hierarchy or mapping file
-	mapping <- read.delim('/Users/mariohg/Dropbox/Norma/data/mapping.txt')[,-2]; mapping
+	metadata <- read.delim('/Users/mariohg/Dropbox/Norma/data/mapping.txt')[,-2]; metadata
 	
 	## Testing different q orders: uncomment the needed
-	hilldiv::div_test(countable = data, qvalue = 0, hierarchy = mapping) # not significantly different
-	hilldiv::div_test(countable = data, qvalue = 1, hierarchy = mapping) # not significantly different
-	hilldiv::div_test(countable = data, qvalue = 2, hierarchy = mapping) # not significantly different
+	hilldiv::div_test(countable = data, qvalue = 0, hierarchy = metadata) # not significantly different
+	hilldiv::div_test(countable = data, qvalue = 1, hierarchy = metadata) # not significantly different
+	hilldiv::div_test(countable = data, qvalue = 2, hierarchy = metadata) # not significantly different
 	
 		
 	
